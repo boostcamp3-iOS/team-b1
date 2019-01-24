2399ac034c471099864c6c6c566857dc08958a6e
조중현 <wndzlf@gmail.com>
2019-01-24 17:11:06 +0900

    메인뷰 추가 완료
    #10 추가 완료  ,  작업 남음

---
+++ UberEats/UberEats/firstItemView/ExpectTimeCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ExpectTimeCollectionViewCell.swift
+//  uberEats
+//
+//  Created by admin on 24/01/2019.
+//  Copyright © 2019 team-b1. All rights reserved.
+//
+
+import UIKit
+
+class ExpectTimeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ UberEats/UberEats/firstItemView/RecommendTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  RecommendTableViewCell.swift
+//  uberEats
+//
+//  Created by admin on 24/01/2019.
+//  Copyright © 2019 team-b1. All rights reserved.
+//
+
+import UIKit
+
+class RecommendTableViewCell: UITableViewCell, UICollectionViewDelegate {
+
+    @IBOutlet var collectionView: UICollectionView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
