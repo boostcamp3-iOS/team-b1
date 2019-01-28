e55ed453ef3061823e999a8ee1fed7bcaf80e199
wndzlf <wndzlf@gmail.com>
2019-01-28 14:03:10 +0900

    addTabBar (#20)
    add TabbarVC and NavigationController

--- UberEats/UberEats/TabBarVC.swift
+++ UberEats/UberEats/TabBarVC.swift
@@ -14,27 +14,24 @@ class TabBarVC: UITabBarController {
         super.viewDidLoad()
         
         let ItemViewStoryBoard = UIStoryboard(name: "ItemView", bundle: nil)
-        let firstViewController = ItemViewStoryBoard.instantiateViewController(withIdentifier: "itemView")
-        firstViewController.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "icMain"), selectedImage: UIImage(named: "1"))
+        let firstViewController = ItemViewStoryBoard.instantiateViewController(withIdentifier: "NavigationVC")
+        firstViewController.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "icons8-home-50"), selectedImage: UIImage(named: "icons8-home-50"))
         
-        let tabBarList = [firstViewController]
+        let mainStoryBoard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let secondViewController = mainStoryBoard.instantiateViewController(withIdentifier: "searchView")
+        secondViewController.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "icons8-home-50"), selectedImage: UIImage(named: "icons8-home-50"))
+        
+        let thirdViewController = mainStoryBoard.instantiateViewController(withIdentifier: "orderHistory")
+        thirdViewController.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "icons8-home-50"), selectedImage: UIImage(named: "icons8-home-50"))
+        
+        let fourthViewController = mainStoryBoard.instantiateViewController(withIdentifier: "myPage")
+        fourthViewController.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "icons8-home-50"), selectedImage: UIImage(named: "icons8-home-50"))
+        
+        let tabBarList = [firstViewController, secondViewController, thirdViewController , fourthViewController]
+        
         self.viewControllers = tabBarList
-    
-        //self.view.superview?.addSubview(self.tabBar)
-        //self.view.addSubview(self.tabBar)
-        //self.view.bringSubviewToFront(self.tabBar)
-        //self.view.sendSubviewToBack(self.tabBar)
-        
-        self.tabBar.barTintColor = #colorLiteral(red: 1, green: 0.1187493313, blue: 0.5523979556, alpha: 1)
-        self.tabBar.layer.borderColor = UIColor.clear.cgColor
-        self.tabBar.barStyle = .black
-        
-        for i in self.view.subviews {
-            print("\(i.description) 탭바 컨트롤러")
-        }
-        
-        print("\(self.view.subviews.count) 탭바 컨트롤러 개수")
-        self.selectedIndex = 1
+
     }
     
     override func viewWillLayoutSubviews() {
