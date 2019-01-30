03864b4d02a1a5428317ae036e5a3f99f1ebee5a
조중현 <wndzlf@gmail.com>
2019-01-30 16:53:12 +0900

    feature/Mainview section5 autolayout
    #35 section5 autolayout 적용중

---
+++ UberEats/UberEats/firstItemView/SeeMoreRestTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SeeMoreRestTableViewCell.swift
+//  UberEats
+//
+//  Created by admin on 30/01/2019.
+//  Copyright © 2019 team-b1. All rights reserved.
+//
+
+import UIKit
+
+class SeeMoreRestTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
