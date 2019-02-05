d946840ca2c2e40c1b167ad65b0f1ecbc5e5b43d
조중현 <wndzlf@gmail.com>
2019-02-05 22:26:39 +0900

    Firebase Chatting and obserData(chatting Data)
    #42

--- UberEats/UberEats/ChattingView/ChatMessageCell.swift
+++ UberEats/UberEats/ChattingView/ChatMessageCell.swift
@@ -1,24 +1,22 @@
 import UIKit
 
-internal class ChatMesageCell: UICollectionViewCell {
+class ChatMesageCell: UICollectionViewCell {
     
     let textFieldView: UITextView = {
         let textView = UITextView()
         textView.text = "Sample Text"
         textView.font = UIFont.systemFont(ofSize: 16)
-        textView.backgroundColor = .clear
-        textView.textColor = .black
+        textView.isScrollEnabled = false
         textView.translatesAutoresizingMaskIntoConstraints = false
         return textView
     }()
     
     let chatBubbleView: UIView = {
-        let bubbleView = UIView()
-        bubbleView.backgroundColor = UIColor(red: 0.10, green: 0.60, blue: 0.91, alpha: 1.0)
-        bubbleView.layer.cornerRadius = 10
-        bubbleView.layer.masksToBounds = true
-        bubbleView.translatesAutoresizingMaskIntoConstraints = false
-        return bubbleView
+        let chatBubbleView = UIView()
+        chatBubbleView.layer.cornerRadius = 10
+        chatBubbleView.layer.masksToBounds = true
+        chatBubbleView.translatesAutoresizingMaskIntoConstraints = false
+        return chatBubbleView
     }()
     
     let isReadLabel: UILabel = {
@@ -27,30 +25,70 @@ internal class ChatMesageCell: UICollectionViewCell {
         return isRead
     }()
     
-    private var bubbleViewWidhAnchor: NSLayoutConstraint?
-    private var bubbleViewRightAnchor: NSLayoutConstraint?
-    private var bubbleViewLeftAnchor: NSLayoutConstraint?
+    var bubbleViewWidhAnchor: NSLayoutConstraint?
+    var bubbleViewTrailingAnchor: NSLayoutConstraint?
+    var bubbleViewLeadingAnchor: NSLayoutConstraint?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         addSubview(chatBubbleView)
         addSubview(textFieldView)
-        addSubview(isReadLabel)
         
-        NSLayoutConstraint.activate([
-            chatBubbleView.topAnchor.constraint(equalTo: topAnchor),
-            chatBubbleView.heightAnchor.constraint(equalTo: heightAnchor),
-            bubbleViewRightAnchor = chatBubbleView.rightAnchor.constraint(equalTo: rightAnchor, constant: -3)
-            
-            
-            ])
+        chatBubbleView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        chatBubbleView.heightAnchor.constraint(equalTo: heightAnchor).isActive = true
+        bubbleViewWidhAnchor = chatBubbleView.widthAnchor.constraint(equalToConstant: 300)
+        bubbleViewLeadingAnchor = chatBubbleView.leadingAnchor.constraint(equalTo: leadingAnchor,
+                                                                          constant: 3)
+        bubbleViewTrailingAnchor = chatBubbleView.trailingAnchor.constraint(equalTo: trailingAnchor,
+                                                                            constant: -10)
         
+        bubbleViewWidhAnchor?.isActive = true
+        bubbleViewLeadingAnchor?.isActive = true
+            
+        textFieldView.leadingAnchor.constraint(equalTo: chatBubbleView.leadingAnchor,
+                                               constant:10).isActive = true
+        textFieldView.trailingAnchor.constraint(equalTo: chatBubbleView.trailingAnchor,
+                                                constant: -8).isActive = true
+        textFieldView.topAnchor.constraint(equalTo: chatBubbleView.topAnchor).isActive = true
+        textFieldView.bottomAnchor.constraint(equalTo: chatBubbleView.bottomAnchor).isActive = true
         
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
+    func fromEmail(userEmail: String) {
+        if userEmail == "user@gmail.com" {
+            textFieldView.backgroundColor = UIColor(displayP3Red: 10 / 255,
+                                                    green: 128 / 255,
+                                                    blue: 2 / 255,
+                                                    alpha: 1.0)
+            textFieldView.textColor = .white
+            
+            chatBubbleView.backgroundColor = UIColor(displayP3Red: 10 / 255,
+                                                     green: 128 / 255,
+                                                     blue: 2 / 255,
+                                                     alpha: 1.0)
+            
+            chatBubbleView.layer.maskedCorners = [.layerMaxXMinYCorner,
+                                                  .layerMinXMaxYCorner,
+                                                  .layerMinXMinYCorner]
+            
+            bubbleViewLeadingAnchor?.isActive = false
+            bubbleViewTrailingAnchor?.isActive = true
+        } else {
+            textFieldView.backgroundColor = .blue
+            textFieldView.textColor = .white
+            chatBubbleView.backgroundColor = .blue
+            chatBubbleView.layer.maskedCorners = [.layerMaxXMinYCorner,
+                                                  .layerMaxXMaxYCorner,
+                                                  .layerMinXMaxYCorner]
+            
+            bubbleViewLeadingAnchor?.isActive = true
+            bubbleViewTrailingAnchor?.isActive = false
+        }
+    }
 }
+
--- UberEats/UberEats/ChattingView/ChattingViewController.swift
+++ UberEats/UberEats/ChattingView/ChattingViewController.swift
@@ -14,49 +14,91 @@ import Firebase
 class ChattingViewController: UIViewController {
     
     @IBOutlet private var deliveryInfoVIew: UIView!
-    @IBOutlet private var chattingCollecionView: UICollectionView!
+    @IBOutlet weak var chattingCollecionView: UICollectionView!
     @IBOutlet private var messageTextField: UITextField!
     
-    private static let chatMessageCellId = "chatMessageCellId"
+    private let chatMessageCellId = "chatMessageCellId"
     
     var messages: [Message] = [
-        Message(text: "안녕하세요", userEmail: "user@gmail.com"),
-        Message(text: "안녕하세요1", userEmail: "user@gmail.com"),
-        Message(text: "안녕하세요2", userEmail: "user@gmail.com"),
-        Message(text: "안녕하세요3", userEmail: "user@gmail.com"),
-        Message(text: "안녕하세요4", userEmail: "delivery@gmail.com"),
-        Message(text: "안녕하세요5", userEmail: "delivery@gmail.com"),
-        Message(text: "안녕하세요6", userEmail: "delivery@gmail.com"),
-        Message(text: "안녕하세요7", userEmail: "delivery@gmail.com"),
-        ]
+//        Message(text: "안녕하세요", userEmail: "user@gmail.com"),
+//        Message(text: "안녕하세요1asdgasdgasddgasdgasddgasdgasdgasdgasdgasdgasdgasdgasdgasdgasdgasdgasdgasdgsagasdgasdgasdgadsgasdgasdgasdgasdgasdgasdgasdg", userEmail: "user@gmail.com"),
+//        Message(text: "안녕하세요2", userEmail: "user@gmail.com"),
+//        Message(text: "안녕하세요3", userEmail: "user@gmail.com"),
+//        Message(text: "안녕하세요4", userEmail: "delivery@gmail.com"),
+//        Message(text: "안녕하세요5", userEmail: "delivery@gmail.com"),
+//        Message(text: "안녕하세요6", userEmail: "delivery@gmail.com"),
+//        Message(text: "안녕하세요7", userEmail: "delivery@gmail.com"),
+    ]
+    
+    private func observeMessages() {
+        let messageFromGroup = Database.database().reference().child("groups").child("-LXbWKQsskziJ50aw8qN")
+        
+        messageFromGroup.observe(.childAdded) { (snapshot) in
+            //print(snapshot.key)
+            let messageRef = Database.database().reference().child("message").child(snapshot.key)
+            messageRef.observe(.value, with: { (messageSnapshot) in
+                if let dictionary = messageSnapshot.value as? [String: Any] {
+                    
+                    guard let text = dictionary["text"] as? String else {return}
+                    guard let userEmail = dictionary["userEmail"] as? String else {return}
+                    //guard let timestamp = dictionary["timestamp"] as? String else {return}
+                    
+                    let messageInstance = Message(text: text, userEmail: userEmail)
+                    self.messages.append(messageInstance)
+                    self.chattingCollecionView.reloadData()
+                    
+//                    print(messageSnapshot)
+                    
+                    self.scrollToBottom()
+                    self.chattingCollecionView.contentInset = UIEdgeInsets(top: 8, left: 0, bottom: 58, right: 0)
+                }
+            })
+
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupChattingCollectionView()
+        observeMessages()
+        
     }
     
     private func setupChattingCollectionView(){
+        chattingCollecionView.delegate = self
+        chattingCollecionView.dataSource = self
+        chattingCollecionView.register(ChatMesageCell.self, forCellWithReuseIdentifier: chatMessageCellId)
         
         chattingCollecionView.contentInset = UIEdgeInsets(top: 8, left: 0, bottom: 58, right: 0)
         chattingCollecionView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: 50, right: 0)
         
-        chattingCollecionView.delegate = self
-        chattingCollecionView.dataSource = self
-        chattingCollecionView.register(ChatMesageCell.self, forCellWithReuseIdentifier: ChattingViewController.chatMessageCellId)
-        
-        let indexPathOfMessage = IndexPath(item: self.messages.count - 2, section: 0)
-       // chattingCollecionView.scrollToItem(at: indexPathOfMessage, at: .bottom, animated: true)
-        
-        chattingCollecionView.reloadData()
         chattingCollecionView.alwaysBounceVertical = true
+        let indexPathOfMessage = IndexPath(item: self.messages.count - 1, section: 0)
         
-        let cellSize = CGSize(width: 300, height: 500)
+        chattingCollecionView.scrollToItem(at: indexPathOfMessage, at: .bottom, animated: true)
+        chattingCollecionView.reloadData()
+        
+
+        let cellSize = CGSize(width: 300, height: 300)
         let layout = UICollectionViewFlowLayout()
-        
+
         layout.itemSize = cellSize
-        chattingCollecionView.setCollectionViewLayout(layout, animated: true)
+        //chattingCollecionView.setCollectionViewLayout(layout, animated: true)
+        //self.scrollToBottom()
     }
+    
+    override func viewDidLayoutSubviews() {
+        //self.scrollToBottom()
+    }
+    
+    func scrollToBottom() {
+        DispatchQueue.main.async {
+            let lastIndex = IndexPath(item: self.messages.count-1, section: 0)
+            self.chattingCollecionView.scrollToItem(at: lastIndex, at: .bottom, animated: false)
+        }
+    }
+    
     
     @IBAction private func sendMessages(_ sender: Any) {
         let messageRef = FirebaseDataService.instance.messageRef.childByAutoId()
@@ -75,40 +117,65 @@ class ChattingViewController: UIViewController {
                 print(error as Any)
                 return
             }
-            
             guard let postData: Dictionary<String, AnyObject> = [ref.key:1] as? [String: AnyObject] else {return}
             Database.database().reference().child("groups").child("-LXbWKQsskziJ50aw8qN").updateChildValues(postData)
         }
+        
+
     }
     
     @IBAction private func loginAction(_ sender: Any) {
         
     }
+    
+    private func estimateFrameForText(text: String) -> CGRect {
+        let size = CGSize(width: 200, height: 1000)
+        let option = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+        
+        return NSString(string: text).boundingRect(with: size, options: option, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16)], context: nil)
+    }
+    
+//    private func identifyUserDelivery(cell: ChatMesageCell, message: Message) {
+//        
+//    }
+    
 }
 
 extension ChattingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 0
+        return 1
     }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return messages.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ChattingViewController.chatMessageCellId, for: indexPath) as? ChatMesageCell else {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: chatMessageCellId, for: indexPath) as? ChatMesageCell else {
             return .init()
         }
-        
-        
         let message = messages[indexPath.item]
+        guard let fromEmail = message.userEmail else {return .init()}
         
-        cell.textFieldView.text = message.text
-        print(cell.textFieldView.text)
-        
-        
+        if let text = messages[indexPath.item].text {
+            cell.textFieldView.text = text
+            cell.bubbleViewWidhAnchor?.constant = estimateFrameForText(text: text).width + 32
+        }
+        cell.fromEmail(userEmail: fromEmail)
+        cell.textFieldView.text = message.text!
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var height: CGFloat = 80
+        //get estimated height
+        if let text = messages[indexPath.row].text {
+            height = estimateFrameForText(text: text).height
+        }
+        return CGSize(width: view.frame.width, height: height+15)
+    }
 }
+
 
 
 
