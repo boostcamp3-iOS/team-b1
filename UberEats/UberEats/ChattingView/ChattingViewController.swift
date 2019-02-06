7097229085aee437980a39f96dc60e98836f0f50
조중현 <wndzlf@gmail.com>
2019-02-06 15:53:49 +0900

    keyBoardwillshow , keyBoardwillHide 에 따라 view frame 변화
    #42 keyBoardwillshow, keyboardwillhide에 따라 view의 frame을 바꾼다.

--- UberEats/UberEats/ChattingView/ChattingViewController.swift
+++ UberEats/UberEats/ChattingView/ChattingViewController.swift
@@ -5,30 +5,19 @@
 //  Created by admin on 31/01/2019.
 //  Copyright © 2019 team-b1. All rights reserved.
 //
-
 import UIKit
 import Firebase
 
 // swiftlint:disable all
 // 채팅방에서 delivertUID를 알고 있고 서로 1대일 채팅 한다는 가정을 하자.
 class ChattingViewController: UIViewController {
-    
-    @IBOutlet private var deliveryInfoVIew: UIView!
-    @IBOutlet weak var chattingCollecionView: UICollectionView!
-    @IBOutlet private var messageTextField: UITextField!
+    @IBOutlet private weak var deliveryInfoVIew: UIView!
+    @IBOutlet private weak var chattingCollecionView: UICollectionView!
+    @IBOutlet private weak var messageTextField: UITextField!
     
     private let chatMessageCellId = "chatMessageCellId"
     
-    var messages: [Message] = [
-//        Message(text: "안녕하세요", userEmail: "user@gmail.com"),
-//        Message(text: "안녕하세요1asdgasdgasddgasdgasddgasdgasdgasdgasdgasdgasdgasdgasdgasdgasdgasdgasdgasdgsagasdgasdgasdgadsgasdgasdgasdgasdgasdgasdgasdg", userEmail: "user@gmail.com"),
-//        Message(text: "안녕하세요2", userEmail: "user@gmail.com"),
-//        Message(text: "안녕하세요3", userEmail: "user@gmail.com"),
-//        Message(text: "안녕하세요4", userEmail: "delivery@gmail.com"),
-//        Message(text: "안녕하세요5", userEmail: "delivery@gmail.com"),
-//        Message(text: "안녕하세요6", userEmail: "delivery@gmail.com"),
-//        Message(text: "안녕하세요7", userEmail: "delivery@gmail.com"),
-    ]
+    private var messages: [Message] = []
     
     private func observeMessages() {
         let messageFromGroup = Database.database().reference().child("groups").child("-LXbWKQsskziJ50aw8qN")
@@ -47,13 +36,12 @@ class ChattingViewController: UIViewController {
                     self.messages.append(messageInstance)
                     self.chattingCollecionView.reloadData()
                     
-//                    print(messageSnapshot)
                     
                     self.scrollToBottom()
                     self.chattingCollecionView.contentInset = UIEdgeInsets(top: 8, left: 0, bottom: 58, right: 0)
                 }
             })
-
+            
         }
     }
     
@@ -61,11 +49,49 @@ class ChattingViewController: UIViewController {
         super.viewDidLoad()
         
         setupChattingCollectionView()
+        setupTextFieldNoti()
         observeMessages()
+        
+        messageTextField.dropShadow(color: .gray, offSet: CGSize.zero)
+    }
+    private func setupTextFieldNoti() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(_ notification: NSNotification) {
+        adjustingHeight(show: true, notification: notification)
+    }
+    
+    @objc private func keyboardWillHide(_ notification: NSNotification) {
+        adjustingHeight(show: false, notification: notification)
+    }
+    
+    func adjustingHeight(show:Bool, notification:NSNotification) {
+        // 1
+        var userInfo = notification.userInfo!
+        // 2
+        let keyboardFrame:CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        // 3
+        let animationDurarion = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
+        // 4
+        var changeInHeight = (keyboardFrame.height) * (show ? -1 : 1)
+        
+        if changeInHeight > 0 {
+           changeInHeight -= 37
+        }
+        
+        //5
+        UIView.animate(withDuration: animationDurarion, animations: { () -> Void in
+            //self.bottomConstraint.constant += changeInHeight
+            self.view.frame.origin.y += changeInHeight
+        })
         
     }
     
-    private func setupChattingCollectionView(){
+    private func setupChattingCollectionView() {
         chattingCollecionView.delegate = self
         chattingCollecionView.dataSource = self
         chattingCollecionView.register(ChatMesageCell.self, forCellWithReuseIdentifier: chatMessageCellId)
@@ -79,10 +105,9 @@ class ChattingViewController: UIViewController {
         chattingCollecionView.scrollToItem(at: indexPathOfMessage, at: .bottom, animated: true)
         chattingCollecionView.reloadData()
         
-
         let cellSize = CGSize(width: 300, height: 300)
         let layout = UICollectionViewFlowLayout()
-
+        
         layout.itemSize = cellSize
         //chattingCollecionView.setCollectionViewLayout(layout, animated: true)
         //self.scrollToBottom()
@@ -99,7 +124,6 @@ class ChattingViewController: UIViewController {
         }
     }
     
-    
     @IBAction private func sendMessages(_ sender: Any) {
         let messageRef = FirebaseDataService.instance.messageRef.childByAutoId()
         
@@ -107,23 +131,20 @@ class ChattingViewController: UIViewController {
         let deliveryUID = "yJI58ZZYyiZVaatNUrIMaBdNsL42"
         
         let data: Dictionary<String, AnyObject> = [
-            "userEmail": "user@gmail.com" as AnyObject,
+            "userEmail": "@gmail.com" as AnyObject,
             "text" : messageTextField.text as AnyObject,
             "timestamp": NSNumber(value: Date().timeIntervalSince1970)
         ]
         
-        messageRef.updateChildValues(data) { (error, ref) in //
+        messageRef.updateChildValues(data) { (error, ref) in 
             guard error == nil else {
                 print(error as Any)
                 return
             }
-            guard let postData: Dictionary<String, AnyObject> = [ref.key:1] as? [String: AnyObject] else {return}
+            guard let postData: Dictionary<String, AnyObject> = [ref.key:1] as? [String : AnyObject] else {return}
             Database.database().reference().child("groups").child("-LXbWKQsskziJ50aw8qN").updateChildValues(postData)
         }
-        
-
     }
-    
     @IBAction private func loginAction(_ sender: Any) {
         
     }
@@ -132,13 +153,11 @@ class ChattingViewController: UIViewController {
         let size = CGSize(width: 200, height: 1000)
         let option = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
         
-        return NSString(string: text).boundingRect(with: size, options: option, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16)], context: nil)
+        return NSString(string: text).boundingRect(with: size,
+                                                   options: option,
+                                                   attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16)],
+                                                   context: nil)
     }
-    
-//    private func identifyUserDelivery(cell: ChatMesageCell, message: Message) {
-//        
-//    }
-    
 }
 
 extension ChattingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
@@ -150,10 +169,13 @@ extension ChattingViewController: UICollectionViewDelegate, UICollectionViewData
         return messages.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: chatMessageCellId, for: indexPath) as? ChatMesageCell else {
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: chatMessageCellId,
+                                                            for: indexPath) as? ChatMesageCell else {
             return .init()
         }
+        
         let message = messages[indexPath.item]
         guard let fromEmail = message.userEmail else {return .init()}
         
@@ -166,18 +188,88 @@ extension ChattingViewController: UICollectionViewDelegate, UICollectionViewData
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
         var height: CGFloat = 80
         //get estimated height
         if let text = messages[indexPath.row].text {
             height = estimateFrameForText(text: text).height
         }
-        return CGSize(width: view.frame.width, height: height+15)
+        return CGSize(width: view.frame.width, height: height + 15)
     }
 }
 
+extension UITextField {
+    //뷰 라운드 처리 설정
+    func makeRounded(cornerRadius : CGFloat?){
+        if let cornerRad = cornerRadius {
+            self.layer.cornerRadius = cornerRad
+        } else {
+            self.layer.cornerRadius = self.layer.frame.height/2
+        }
+        self.layer.masksToBounds = true
+    }
+    
+    //뷰 그림자 설정
+    //color: 색상, opacity: 그림자 투명도, offset: 그림자 위치, radius: 그림자 크기
+    func dropShadow(color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true) {
+        
+        layer.cornerRadius = 10.0
+        layer.shadowColor = color.cgColor
+        layer.shadowOffset = offSet
+        layer.shadowOpacity = opacity
+        layer.shadowRadius = radius
+        layer.shadowPath = nil
+        layer.shouldRasterize = true
 
+//
+//        layer.masksToBounds = false
+//        layer.shadowColor = color.cgColor
+//        layer.shadowOpacity = opacity
+//        layer.shadowOffset = offSet
+//        layer.shadowRadius = radius
+//        layer.cornerRadius = 10.0
+//
+//        layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+//        layer.shouldRasterize = true
+//        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+    }
+    
+    func dropShadow2(scale: Bool = true) {
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowOffset = CGSize(width: -0.0, height: -3.0)
+        layer.shadowRadius = 1
+        
+        layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+        layer.shouldRasterize = true
+        
+        //layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+    }
+}
 
-
-
-
+extension UITextField {
+    
+    func applyCustomEffect() {
+        self.borderStyle = .none
+        self.backgroundColor = UIColor.groupTableViewBackground // Use anycolor that give you a 2d look.
+        
+        //To apply corner radius
+        self.layer.cornerRadius = self.frame.size.height / 2
+        
+        //To apply border
+        self.layer.borderWidth = 0.25
+        self.layer.borderColor = UIColor.white.cgColor
+        
+        //To apply Shadow
+        self.layer.shadowOpacity = 1
+        self.layer.shadowRadius = 1.0
+        self.layer.shadowOffset = CGSize.zero // Use any CGSize
+        self.layer.shadowColor =
+            UIColor.lightGray.cgColor
+        
+        self.layer.sublayerTransform = CATransform3DMakeTranslation(20, 0, 0)
+    }
+}
