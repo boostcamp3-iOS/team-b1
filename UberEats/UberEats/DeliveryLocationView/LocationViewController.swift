49c44e9b3a5a7c3487423a602653a8470bd4ca53
hyeju <hyeju.tech@gmail.com>
2019-02-08 22:02:20 +0900

    LocationView 코드 리펙토링했습니다.
    #47

--- UberEats/UberEats/DeliveryLocationView/LocationViewController.swift
+++ UberEats/UberEats/DeliveryLocationView/LocationViewController.swift
@@ -11,31 +11,22 @@ import GoogleMaps
 import CoreLocation
 
 class LocationViewController: UIViewController {
+    private let topInset: CGFloat = 450
     private let backButton = UIButton().initButtonWithImage("blackArrow")
     private let moveCurrentLocationButton = UIButton().initButtonWithImage("btCurrentlocation")
-    
-    private let arrivalTimeHeaderId: String = "orderChecking"
-    private let orderNameHeaderId: String = "orderName"
-    private let separatorHeaderId: String = "separator"
-    private let tempHeaderId: String = "tempHeader"
-    
-    private let separatorFooterId: String = "separator"
-    private let totalPriceFooterId: String = "totalPrice"
-    private let tempFooterId: String = "tempFooter"
-    
-    private let orderCancelCellId: String = "orderCancel"
-    private let orderMenuCellId: String = "orderedMenu"
-    private let emptyCellId: String = "emptyCell"
     
     private let orders = ["초콜렛 밀크티 (아이스, 라지) Chocolate Milk Tea (Iced, Large)",
                           "아메리카노 (아이스, 라지) Americano (Iced, Large)",
                           "블랙 밀크티+펄(아이스, 라지) Black Milk Tea+Pearl(Iced, Large)"]
 
+    private let sectionHeader = UICollectionView.elementKindSectionHeader
+    private let sectionFooter = UICollectionView.elementKindSectionFooter
+
     private var mapView: GMSMapView?
     private let locationManager = CLLocationManager()
     private var userLocation = CLLocationCoordinate2D(latitude: 37.49646975398706, longitude: 127.02905088660754)
     
-    let orderDetailCollectionView: UICollectionView = {
+    lazy var orderDetailCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.minimumInteritemSpacing = 0
         layout.minimumLineSpacing = 0
@@ -43,7 +34,7 @@ class LocationViewController: UIViewController {
                                               collectionViewLayout: layout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.backgroundColor = #colorLiteral(red: 0.9525781274, green: 0.9469152093, blue: 0.9569309354, alpha: 1)
-        collectionView.contentInset = UIEdgeInsets(top: 450, left: 10, bottom: 0, right: 10)
+        collectionView.contentInset = UIEdgeInsets(top: self.topInset, left: 10, bottom: 0, right: 10)
         collectionView.showsVerticalScrollIndicator = false
         return collectionView
     }()
@@ -68,15 +59,13 @@ class LocationViewController: UIViewController {
             locationManager.distanceFilter = 500.0
             locationManager.startUpdatingLocation()
         }
-        
-        GMSServices.provideAPIKey("AIzaSyAphHHY5LL8tq4QOepg2cCFASelCbSLa0E")
+
         // user와 deliverer의 중간 지점으로 설정할 것
         let camera = GMSCameraPosition(latitude: (userLocation.latitude + 37.499862) / 2,
                                        longitude: (userLocation.longitude + 127.030378) / 2,
                                        zoom: 17)
         mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView?.padding = UIEdgeInsets(top: 0, left: 10, bottom: self.view.frame.height - 500 + 10, right: 0)
-//        mapView
         orderDetailCollectionView.backgroundView = mapView
         
         let userMarker = GMSMarker(position: userLocation)
@@ -116,55 +105,55 @@ class LocationViewController: UIViewController {
             moveCurrentLocationButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,
                                                                 constant: -15),
             moveCurrentLocationButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
-                                                              constant: 450 - 10),
+                                                              constant: topInset - 10),
             moveCurrentLocationButton.widthAnchor.constraint(equalToConstant: 32),
             moveCurrentLocationButton.heightAnchor.constraint(equalToConstant: 32)
             ])
     }
     
     private func setupCollectionView() {
+        //headers
         let arrivalTimeHeaderNib = UINib(nibName: "OrderCheckingCollectionReusableView",
                                          bundle: nil)
         
-        //headers
         orderDetailCollectionView.register(arrivalTimeHeaderNib,
-                                           forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
-                                           withReuseIdentifier: arrivalTimeHeaderId)
+                                           forSupplementaryViewOfKind: sectionHeader,
+                                           withReuseIdentifier: Identifiers.arrivalTimeHeaderId)
         
         orderDetailCollectionView.register(OrderNameCollectionReusableView.self,
-                                           forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
-                                           withReuseIdentifier: orderNameHeaderId)
+                                           forSupplementaryViewOfKind: sectionHeader,
+                                           withReuseIdentifier: Identifiers.orderNameHeaderId)
         
         orderDetailCollectionView.register(TempCollectionReusableView.self,
-                                           forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
-                                           withReuseIdentifier: tempHeaderId)
+                                           forSupplementaryViewOfKind: sectionHeader,
+                                           withReuseIdentifier: Identifiers.tempHeaderId)
         
         orderDetailCollectionView.register(SeparatorCollectionReusableView.self,
-                                           forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
-                                           withReuseIdentifier: separatorHeaderId)
+                                           forSupplementaryViewOfKind: sectionHeader,
+                                           withReuseIdentifier: Identifiers.separatorHeaderId)
         
         //footers
         orderDetailCollectionView.register(SeparatorCollectionReusableView.self,
-                                           forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter,
-                                           withReuseIdentifier: separatorFooterId)
+                                           forSupplementaryViewOfKind: sectionFooter,
+                                           withReuseIdentifier: Identifiers.separatorFooterId)
         
         orderDetailCollectionView.register(TotalPriceCollectionReusableView.self,
-                                           forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter,
-                                           withReuseIdentifier: totalPriceFooterId)
+                                           forSupplementaryViewOfKind: sectionFooter,
+                                           withReuseIdentifier: Identifiers.totalPriceFooterId)
         
         orderDetailCollectionView.register(TempCollectionReusableView.self,
-                                           forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter,
-                                           withReuseIdentifier: tempFooterId)
+                                           forSupplementaryViewOfKind: sectionFooter,
+                                           withReuseIdentifier: Identifiers.tempFooterId)
         
         //cells
         orderDetailCollectionView.register(OrderCancelCollectionViewCell.self,
-                                           forCellWithReuseIdentifier: orderCancelCellId)
+                                           forCellWithReuseIdentifier: Identifiers.orderCancelCellId)
         
         orderDetailCollectionView.register(OrderedMenuCollectionViewCell.self,
-                                           forCellWithReuseIdentifier: orderMenuCellId)
+                                           forCellWithReuseIdentifier: Identifiers.orderMenuCellId)
         
         orderDetailCollectionView.register(EmptyCollectionViewCell.self,
-                                           forCellWithReuseIdentifier: emptyCellId)
+                                           forCellWithReuseIdentifier: Identifiers.emptyCellId)
         
     }
     
@@ -182,20 +171,9 @@ class LocationViewController: UIViewController {
 
 extension LocationViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
         let currentScroll = scrollView.contentOffset.y
         
-        if currentScroll > -150 {
-            mapView?.alpha = 0
-        } else {
-            
-            mapView?.alpha = -currentScroll / 450
-        }
-        print("scroll \(currentScroll)")
-//        scrollView.backgroundColor = UIColor(red: 0.8039215803,
-//                                             green: 0.8039215803,
-//                                             blue: 0.8039215803,
-//                                             alpha: currentScroll)
+        mapView?.alpha = currentScroll > -150 ? 0 : (-currentScroll / topInset)
     }
 }
 
@@ -221,21 +199,25 @@ extension LocationViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        switch indexPath.section {
-        case 0:
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: orderCancelCellId,
+        guard let section = SectionName(rawValue: indexPath.section) else {
+            return .init()
+        }
+        
+        switch section {
+        case .timeDetail:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Identifiers.orderCancelCellId,
                                                           for: indexPath)
             return cell
-        case 1:
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: orderMenuCellId,
+        case .orders:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Identifiers.orderMenuCellId,
                                                                 for: indexPath) as? OrderedMenuCollectionViewCell else {
                 return .init()
             }
             cell.orderMenuLabel.text = orders[indexPath.item]
             
             return cell
-        default:
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: emptyCellId, for: indexPath)
+        case .sale:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Identifiers.emptyCellId, for: indexPath)
             cell.backgroundColor = .green
             return cell
         }
@@ -244,46 +226,36 @@ extension LocationViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView,
                         viewForSupplementaryElementOfKind kind: String,
                         at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        guard let section = SectionName(rawValue: indexPath.section) else {
+            return .init()
+        }
+        
+        var identifier = ""
+        
         if kind == UICollectionView.elementKindSectionHeader {
-            switch indexPath.section {
-            case 0:
-                let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
-                                                                             withReuseIdentifier: arrivalTimeHeaderId,
-                                                                             for: indexPath)
-                return header
-            case 1:
-                let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
-                                                                             withReuseIdentifier: orderNameHeaderId,
-                                                                             for: indexPath)
-                return header
-            case 2:
-                let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
-                                                                             withReuseIdentifier: separatorHeaderId,
-                                                                             for: indexPath)
-                return header
-            default:
-                return collectionView.dequeueReusableSupplementaryView(ofKind: kind,
-                                                                       withReuseIdentifier: tempHeaderId,
-                                                                       for: indexPath)
+            switch section {
+            case .timeDetail:
+                identifier = Identifiers.arrivalTimeHeaderId
+            case .orders:
+                identifier = Identifiers.orderNameHeaderId
+            case .sale:
+                identifier = Identifiers.separatorHeaderId
             }
         } else {
-            switch indexPath.section {
-            case 0:
-                let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
-                                                                             withReuseIdentifier: separatorFooterId,
-                                                                             for: indexPath)
-                return footer
-            case 1:
-                let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
-                                                                             withReuseIdentifier: totalPriceFooterId,
-                                                                             for: indexPath)
-                return footer
-            default:
-                return collectionView.dequeueReusableSupplementaryView(ofKind: kind,
-                                                                       withReuseIdentifier: tempFooterId,
-                                                                       for: indexPath)
+            switch section {
+            case .timeDetail:
+                identifier = Identifiers.separatorFooterId
+            case .orders:
+                identifier = Identifiers.totalPriceFooterId
+            case .sale:
+                identifier = Identifiers.tempFooterId
             }
         }
+        
+        return collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                               withReuseIdentifier: identifier,
+                                                               for: indexPath)
     }
 }
 
@@ -291,12 +263,17 @@ extension LocationViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         referenceSizeForHeaderInSection section: Int) -> CGSize {
+        
+        guard let section = SectionName(rawValue: section) else {
+            return .init(width: 0, height: 0)
+        }
+        
         switch section {
-        case 0:
+        case .timeDetail:
             return .init(width: self.view.frame.width - 20, height: 200)
-        case 1:
+        case .orders:
             return .init(width: self.view.frame.width - 20, height: 60)
-        default:
+        case .sale:
             return .init(width: self.view.frame.width - 20, height: 10)
         }
     }
@@ -304,10 +281,15 @@ extension LocationViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         referenceSizeForFooterInSection section: Int) -> CGSize {
+        
+        guard let section = SectionName(rawValue: section) else {
+            return .init(width: 0, height: 0)
+        }
+        
         switch section {
-        case 1:
+        case .orders:
             return .init(width: self.view.frame.width - 20, height: 50)
-        default:
+        case .timeDetail, .sale:
             return .init(width: self.view.frame.width - 20, height: 10)
         }
     }
@@ -317,12 +299,17 @@ extension LocationViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
-        switch indexPath.section {
-        case 0:
+        
+        guard let section = SectionName(rawValue: indexPath.section) else {
+            return .init(width: 0, height: 0)
+        }
+        
+        switch section {
+        case .timeDetail:
             return .init(width: self.view.frame.width - 20, height: 40)
-        case 1:
+        case .orders:
             return .init(width: self.view.frame.width - 20, height: 65)
-        default:
+        case .sale:
             return .init(width: self.view.frame.width - 20, height: 100)
         }
     }
@@ -335,10 +322,4 @@ extension LocationViewController: CLLocationManagerDelegate {
         print("locations = \(locValue.latitude) \(locValue.longitude)")
 //        self.userLocation = locValue
     }
-}
-
-private enum SectionName: Int {
-    case timeDetail = 0
-    case orders
-    case sale
 }
