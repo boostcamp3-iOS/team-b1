024e33fd171a1c5374a05f0a74e987121729a72d
hyeju <hyeju.tech@gmail.com>
2019-02-10 15:24:13 +0900

    make LocationView
    #47
    - mapView 추가
    - collectionView 추가
    collectionView 수정중
    
    #47
    - 주문 예상 시간을 보여줄 orderCheckingHeader 추가
    - 주문 취소 셀 orderCancelCell 추가
    - 선택한 메뉴들 orderedMenuCell 추가
    LocationView collectionView 수정
    
    #47
    - separator Footer 추가
    - 주문음식목록 주문이름 orderName Header 추가
    LocationView 수정
    
    #47
    - collectionView에 Footer 추가
    - backButton 추가
    LocationView 임시로 tabBar에 추가
    
    #47
    LocationView 임시로 tabBar 두번째 화면에 추가해뒀습니다
    deliveryLocationView 수정
    
    #47
    - mapView -> GMSMapView(googleMap)로 변경
    - 포커즈를 두 위치의 사이로 이동시키게 수정
    - mapView와 CollectionView 코드로 수정
    - 위치 권한 요청 코드 추가
    locationView Map 수정하였습니다.
    
    #47
    - mapView를 collectionView의 backgroundView로 추가하였습니다.
    - 현재 위치로 이동하는 버튼 위치 수정하였습니다.
    - 스크롤시 배경 불투명하게 수정하였습니다.
    - Google 로고 위치 수정하였습니다.
    - 두번째 섹션 푸터를 수정해서 세번째 헤더로 추가하였습니다.
    make LocationView
    
    #47
    - mapView 추가
    - collectionView 추가
    collectionView 수정중
    
    #47
    - 주문 예상 시간을 보여줄 orderCheckingHeader 추가
    - 주문 취소 셀 orderCancelCell 추가
    - 선택한 메뉴들 orderedMenuCell 추가
    LocationView collectionView 수정
    
    #47
    - separator Footer 추가
    - 주문음식목록 주문이름 orderName Header 추가
    LocationView 수정
    
    #47
    - collectionView에 Footer 추가
    - backButton 추가
    LocationView 임시로 tabBar에 추가
    
    #47
    LocationView 임시로 tabBar 두번째 화면에 추가해뒀습니다
    deliveryLocationView 수정
    
    #47
    - mapView -> GMSMapView(googleMap)로 변경
    - 포커즈를 두 위치의 사이로 이동시키게 수정
    - mapView와 CollectionView 코드로 수정
    - 위치 권한 요청 코드 추가
    update README.md
    
    - 이미지 추가 및 내용 순서 수정
    update README.md
    
    - 이미지 수정
    update README.md
    
    - 이미지 추가 및 내용 순서 수정
    update README.md
    
    - 이미지 수정
    make LocationView
    
    #47
    - mapView 추가
    - collectionView 추가
    collectionView 수정중
    
    #47
    - 주문 예상 시간을 보여줄 orderCheckingHeader 추가
    - 주문 취소 셀 orderCancelCell 추가
    - 선택한 메뉴들 orderedMenuCell 추가
    LocationView collectionView 수정
    
    #47
    - separator Footer 추가
    - 주문음식목록 주문이름 orderName Header 추가
    LocationView 수정
    
    #47
    - collectionView에 Footer 추가
    - backButton 추가
    LocationView 임시로 tabBar에 추가
    
    #47
    LocationView 임시로 tabBar 두번째 화면에 추가해뒀습니다
    deliveryLocationView 수정
    
    #47
    - mapView -> GMSMapView(googleMap)로 변경
    - 포커즈를 두 위치의 사이로 이동시키게 수정
    - mapView와 CollectionView 코드로 수정
    - 위치 권한 요청 코드 추가
    update README.md
    
    - 이미지 추가 및 내용 순서 수정
    update README.md
    
    - 이미지 수정
    rebase 위한 commit

--- UberEats/UberEats/AppDelegate.swift
+++ UberEats/UberEats/AppDelegate.swift
@@ -18,14 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-
-        
-//        window = UIWindow(frame: UIScreen.main.bounds)
-//
-//        window?.rootViewController = TabBarVC()
-//
-//        window?.makeKeyAndVisible()
-        
         FirebaseApp.configure()
         GMSServices.provideAPIKey("AIzaSyAphHHY5LL8tq4QOepg2cCFASelCbSLa0E")
         
