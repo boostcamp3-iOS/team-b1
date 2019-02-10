c77671d70b96ff8aae73e7842d831ff653abf56f
hyeju <hyeju.tech@gmail.com>
2019-02-10 21:02:31 +0900

    문의하기 버튼 추가
    뷰 구성에서 문의하기 버튼이 빠져서 추가했습니다!

--- UberEats/UberEats/Scene/DeliveryLocation/View/EmptyCollectionViewCell.swift
+++ UberEats/UberEats/Scene/DeliveryLocation/View/EmptyCollectionViewCell.swift
@@ -9,5 +9,12 @@
 import UIKit
 
 class EmptyCollectionViewCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+    }
 
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
