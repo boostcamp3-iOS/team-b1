6183338fcffab8f8ded5536124e886185ddac937
GongUi <zhanggoni@gmail.com>
2019-02-17 16:21:16 +0900

    카트 화면 2차 개발 작업 (#83)
    - 이전 화면에서 상점 정보, 배송지 정보, 음식 정보 넘겨줄 수 있도록 수정 
    - 주문 버튼 이벤트 받을 수 있도록 수정
    #82

---
+++ UberEats/UberEats/Scene/Cart/Model/CartModels.swift
@@ -0,0 +1,93 @@
+//
+//  CartTableViewCellModels.swift
+//  UberEats
+//
+//  Created by 장공의 on 10/02/2019.
+//  Copyright © 2019 team-b1. All rights reserved.
+//
+
+import UIKit
+
+struct CartModel {
+    let storeInfo: StoreInfoModel
+    let deilveryInfo: DeilveryInfoModel
+    let foodOrderedInfo: FoodsOrderedByOthersInfoModel?
+}
+
+struct StoreInfoModel {
+    let name: String
+    let deliveryTime: String
+}
+
+struct DeilveryInfoModel {
+    let locationImage: String
+    let detailedAddress: String
+    let address: String
+    let deliveryMethod: DeliveryMethod
+    let roomNumber: Int
+}
+
+struct FoodsOrderedByOthersInfoModel {
+
+}
+
+struct OrderInfoModel {
+    let amount: Int
+    let orderName: String
+    let price: Int
+}
+
+enum DeliveryMethod {
+    case deliveryToDoor
+    case pickUpOutside
+}
+
+enum CartItemModelType {
+    case storeInfo(StoreInfoModel)
+    case deliveryInfo(DeilveryInfoModel)
+    case foodsOrderedByOthersInfo(FoodsOrderedByOthersInfoModel)
+    case orderBookTitleInfo()
+    case order(OrderInfoModel)
+    case memo
+    case priceInfo
+    case paymentInfo
+    case empty
+}
+
+enum CartSection: Int, CaseIterable {
+
+    case storeInfo = 0
+    case deliveryAddress
+    case foodsOrderedByOthers
+    case orderBookTitle
+    case order
+    case memo
+    case priceInfo
+    case paymentInfo
+    case empty
+
+    var identifier: String {
+        switch self {
+        case .storeInfo:
+            return "storeInfoCell"
+        case .deliveryAddress:
+            return "deliveryAddressCell"
+        case .foodsOrderedByOthers:
+            return "foodsOrderedByOthersCell"
+        case .orderBookTitle:
+            return "orderBookTitleCell"
+        case .order:
+            return "orderCell"
+        case .memo:
+            return "memoCell"
+        case .priceInfo:
+            return "priceInfoCell"
+        case .paymentInfo:
+            return "paymentInfoCell"
+        case .empty:
+            return "emptyCell"
+        }
+
+    }
+
+}
--- UberEats/UberEats/Scene/Cart/ViewController/CartViewController.swift
+++ UberEats/UberEats/Scene/Cart/ViewController/CartViewController.swift
@@ -10,72 +10,92 @@ import UIKit
 
 class CartViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var orderButton: OrderButton!
+
     fileprivate var cartItems: [[CartItemModelType]] =
         [[CartItemModelType]](repeating: [], count: CartSection.allCases.count)
 
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var orderButton: OrderButton!
+    var cartModel: CartModel = CartModel.empty() {
+        didSet {
+            setUpCartModel()
+        }
+    }
+
+    var orderInfoModels: [OrderInfoModel] = [OrderInfoModel]() {
+        didSet {
+            cartItems[CartSection.order.rawValue] = orderInfoModels.map({
+                CartItemModelType.order($0)
+            })
+            tableView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let storeInfo = StoreInfoModel(name: "배스킨라빈스 신천역점 Basikin  Robbins",
-                                       deliveryTime: "20 - 30")
+        setUpDummy()
+        orderButton.orderButtonClickable = self
 
-        let deilveryInfo = DeilveryInfoModel(locationImage: "asdasdasd",
-                                             detailedAddress: "116-11",
-                                             address: "서울특별시 송파구 방이동",
-                                             deliveryMethod: .pickUpOutside,
-                                             roomNumber: 303)
+    }
 
-        let foodOrderedInfo = FoodsOrderedByOthersInfoModel()
+    private func setUpDummy() {
+        cartModel = CartModel.empty()
+        let pancakeOrder = OrderInfoModel(amount: 1,
+                                          orderName: "딸기 수플래 팬케이크 Strawberry Souffle Pancake",
+                                          price: 17000)
 
-        for _ in 1...2 {
-            let pancakeOrder = OrderInfoModel(amount: 1,
-                                              orderName: "딸기 수플래 팬케이크 Strawberry Souffle Pancake",
-                                              price: 17000)
-
-            let waffleOrder = OrderInfoModel(amount: 2,
-                                             orderName: "홍콩 에그와플 Hongkong Egg Waffle",
-                                             price: 13000)
-
-            cartItems[CartSection.order.rawValue].append(
-                CartItemModelType.order(pancakeOrder)
-            )
-            cartItems[CartSection.order.rawValue].append(
-                CartItemModelType.order(waffleOrder)
-            )
-        }
-
-        cartItems[CartSection.storeInfo.rawValue].append(
-            CartItemModelType.storeInfo(storeInfo)
-        )
-        cartItems[CartSection.deliveryAddress.rawValue].append(
-            CartItemModelType.deliveryInfo(deilveryInfo)
-        )
-        cartItems[CartSection.foodsOrderedByOthers.rawValue].append(
-            CartItemModelType.foodsOrderedByOthersInfo(foodOrderedInfo)
-        )
-        cartItems[CartSection.orderBookTitle.rawValue].append(
-            CartItemModelType.orderBookTitleInfo()
-        )
-        cartItems[CartSection.memo.rawValue].append(
-            CartItemModelType.memo
-        )
-        cartItems[CartSection.priceInfo.rawValue].append(
-            CartItemModelType.priceInfo
-        )
-        cartItems[CartSection.paymentInfo.rawValue].append(
-            CartItemModelType.paymentInfo
-        )
-        cartItems[CartSection.empty.rawValue].append(
-            CartItemModelType.empty
-        )
+        orderInfoModels.append(pancakeOrder)
     }
 
     @IBAction func clickedExitButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
+
+    private func setUpCartModel() {
+
+        cartItems[CartSection.storeInfo.rawValue].append(
+            CartItemModelType.storeInfo(cartModel.storeInfo)
+        )
+        cartItems[CartSection.deliveryAddress.rawValue].append(
+            CartItemModelType.deliveryInfo(cartModel.deilveryInfo)
+        )
+
+        if let foodOrderedInfo = cartModel.foodOrderedInfo {
+            cartItems[CartSection.foodsOrderedByOthers.rawValue].append(
+                CartItemModelType.foodsOrderedByOthersInfo(foodOrderedInfo)
+            )
+        }
+
+        cartItems[CartSection.orderBookTitle.rawValue].append(
+            CartItemModelType.orderBookTitleInfo()
+        )
+
+        cartItems[CartSection.memo.rawValue].append(
+            CartItemModelType.memo
+        )
+
+        cartItems[CartSection.priceInfo.rawValue].append(
+            CartItemModelType.priceInfo
+        )
+
+        cartItems[CartSection.paymentInfo.rawValue].append(
+            CartItemModelType.paymentInfo
+        )
+
+        cartItems[CartSection.empty.rawValue].append(
+            CartItemModelType.empty
+        )
+    }
+}
+
+extension CartViewController: OrderButtonClickable {
+
+    func onClickedOrderButton(_ sender: Any) {
+        // TO DO
+        _ = ""
+    }
+
 }
 
 extension CartViewController: UITableViewDataSource, UITableViewDelegate {
@@ -117,56 +137,6 @@ extension CartViewController: UITableViewDataSource, UITableViewDelegate {
         case .memo, .priceInfo, .paymentInfo, .empty:
             return cell
         }
-    }
-
-}
-
-private enum CartItemModelType {
-    case storeInfo(StoreInfoModel)
-    case deliveryInfo(DeilveryInfoModel)
-    case foodsOrderedByOthersInfo(FoodsOrderedByOthersInfoModel)
-    case orderBookTitleInfo()
-    case order(OrderInfoModel)
-    case memo
-    case priceInfo
-    case paymentInfo
-    case empty
-}
-
-private enum CartSection: Int, CaseIterable {
-
-    case storeInfo = 0
-    case deliveryAddress
-    case foodsOrderedByOthers
-    case orderBookTitle
-    case order
-    case memo
-    case priceInfo
-    case paymentInfo
-    case empty
-
-    var identifier: String {
-        switch self {
-        case .storeInfo:
-            return "storeInfoCell"
-        case .deliveryAddress:
-            return "deliveryAddressCell"
-        case .foodsOrderedByOthers:
-            return "foodsOrderedByOthersCell"
-        case .orderBookTitle:
-            return "orderBookTitleCell"
-        case .order:
-            return "orderCell"
-        case .memo:
-            return "memoCell"
-        case .priceInfo:
-            return "priceInfoCell"
-        case .paymentInfo:
-            return "paymentInfoCell"
-        case .empty:
-            return "emptyCell"
-        }
-
     }
 
 }
